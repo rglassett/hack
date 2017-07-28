5e82fe59f9ff4f827a9f8750dc7c2d734a02711a
Ryan Glassett <rglassett@gmail.com>
2017-07-27 22:08:58 -0700

    Add integration tests for Hack::Assembler

---
+++ spec/integration/assembler_spec.rb
@@ -0,0 +1,50 @@
+require 'rspec'
+require 'tempfile'
+require_relative '../../lib/hack'
+
+describe Hack::Assembler do
+  context 'given Hack assembly code with no variables or labels' do
+    it 'correctly generates the Add program' do
+      compare('Add')
+    end
+
+    it 'correctly generates the MaxL program' do
+      compare('MaxL')
+    end
+
+    it 'correctly generates the RectL program' do
+      compare('RectL')
+    end
+
+    it 'correctly generates the PongL program' do
+      compare('PongL')
+    end
+  end
+
+  context 'given Hack assembly code with variables and labels' do
+    it 'correctly generates the Max program' do
+      compare('Max')
+    end
+
+    it 'correctly generates the Rect program' do
+      compare('Rect')
+    end
+
+    it 'correctly generates the Pong program' do
+      compare('Pong')
+    end
+  end
+
+  def compare(program)
+    File.open("spec/fixtures/#{program}.asm") do |input|
+      Tempfile.open('assembler_spec', '/tmp') do |output|
+        Hack::Assembler.new(input, output).assemble
+        [input, output].each(&:rewind)
+
+        File.open("spec/fixtures/#{program}.hack") do |compare_file|
+          expect(output.readlines).to eq(compare_file.readlines)
+        end
+      end
+    end
+  end
+end
