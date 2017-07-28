b4e588e1152a5d476eb72fbfee9b26fd115c3b90
Ryan Glassett <rglassett@gmail.com>
2017-07-27 20:50:00 -0700

    Use Tempfile in Hack::Assembler
    Prevents a permanent file from being written if the assembler encounters
    an error partway through the assembly process.

--- lib/hack/assembler.rb
+++ lib/hack/assembler.rb
@@ -1,17 +1,26 @@
+require 'tempfile'
+require 'pathname'
+require 'fileutils'
+
 module Hack
   class Assembler
     def self.assemble(input_filename)
-      output_filename =
-        File.dirname(input_filename) +
-        File::SEPARATOR +
-        'My' +
-        File.basename(input_filename, '.asm') +
-        '.hack'
-
       File.open(input_filename) do |input|
-        File.open(output_filename, 'w') do |output|
+        with_tempfile(input_filename) do |output|
           self.new(input, output).assemble
         end
+      end
+    end
+
+    def self.with_tempfile(input_filename)
+      input_pathname = Pathname.new(input_filename)
+      basename = input_pathname.basename('.asm')
+      output_filename = input_pathname.dirname.join("My#{basename}.hack")
+
+      Tempfile.open('hack_assembler', '/tmp') do |output|
+        yield output
+        FileUtils.mv(output.path, output_filename)
+        ObjectSpace.undefine_finalizer(output)
       end
     end
 
